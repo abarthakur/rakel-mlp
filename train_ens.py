288f46c965daa2886fb9f6d84a31e3f51460cbe3
Aneesh Barthakur <abarthakur96@gmail.com>
2018-03-27 22:30:40 +0530

    Add code for prediction, calculating overlap, saving epochs trained

--- train_ens.py
+++ train_ens.py
@@ -18,6 +18,17 @@ class Ensemble :
 		self.classifier_list=[]
 		for i in range(0,num_classifiers):
 			self.classifier_list.append(None)
+
+	def expand_ensemble(self,num_classifiers):
+		n1=self.num_classifiers
+		self.num_classifiers=num_classifiers
+		for i in range(n1,num_classifiers):
+			self.classifier_info.append({})
+			self.classifier_info[i]["label_list"]=[]
+			self.classifier_info[i]["pos_tset"]=None
+			self.classifier_info[i]["neg_tset"]=None
+		for i in range(n1,num_classifiers):
+			self.classifier_list.append(None)
 	
 
 	def save(self,foldername):
@@ -50,7 +61,7 @@ class Ensemble :
 		if not dict:
 			print("Failed to load!")
 			return
-		self.num_classifiers=dict["num_classifiers"]
+		# self.num_classifiers=dict["num_classifiers"]
 		self.num_labels=dict["num_labels"]
 		self.num_features=dict["num_features"]
 		self.labels_per_classifier=dict["labels_per_classifier"]
@@ -61,7 +72,10 @@ class Ensemble :
 			self.classifier_list[i]=self.classifier_list[i].load(foldername,i,self.num_features,
 															self.labels_per_classifier)
 			if self.classifier_list[i]:
-				self.classifier_list[i].epochs_trained=dict["classifier_info"][i]["epochs_trained"]
+				try :
+					self.classifier_list[i].epochs_trained=dict["classifier_info"][i]["epochs_trained"]
+				except KeyError:
+					self.classifier_list[i].epochs_trained=0 #should have saved it...
 
 	def predict(self,sample):
 		predictions=np.zeros(self.num_labels)
@@ -69,12 +83,80 @@ class Ensemble :
 			this_classifier=self.classifier_list[i]
 			labels=self.classifier_info[i]["label_list"]
 			p=this_classifier.predict(sample)
+			# p = np.around(p)
+			# print("For each classifier")
+			# print(labels)
+			# print(np.max(p))
 			for i in range(0,len(labels)):
 				predictions[labels[i]]+=p[i]
-		predictions=predictions/np.sum(predictions)
+			# break
+		# predictions=predictions/np.sum(predictions)
+		# print(predictions)
 		return predictions
 
+	def predict_batch(self,sample_batch):
+		num_samples=sample_batch.shape[0]
+		predictions=np.zeros((num_samples,self.num_labels))
+		for i in range(0,sample_batch.shape[0]):
+			prediction= self.predict(sample_batch[i,:])
+			predictions[i,:]=prediction
+		return predictions
 
+	def predict_batch_with_metrics(self,sample_batch,sample_batch_labels,k_values,printResults=True):
+		num_samples=sample_batch.shape[0]
+		predictions=self.predict_batch(sample_batch)
+		#precision at k = # of correctly predicted labels in top k / k
+		metrics={}
+		precision_at_k_values=[]
+		#propensity scored precision at k = # of co
+		# psp_at_k=np.zeros(len(k_values))
+		for k in k_values:
+			p_at_k=0
+			# psp_at_k=0
+			for i in range(0,num_samples):
+				prediction = predictions[i,:]
+				N=prediction.shape[0]
+				top_k_preds=np.argpartition(prediction,N-k)[(N-k):].tolist()
+				# print(top_k_preds)
+				#note : labels are 0-centered
+				intersect = set(top_k_preds) & set(sample_batch_labels[i])
+				val = float(len(intersect))/float(k)
+				p_at_k+=val
+			p_at_k= p_at_k/float(num_samples)
+			precision_at_k_values.append(p_at_k)
+
+		metrics["p_at_k"]=precision_at_k_values
+
+		for j in range(0,len(k_values)):
+			k=k_values[j]
+			print("Precision @ "+str(k)+" : "+str(metrics["p_at_k"][j]))
+
+		return predictions,metrics
+
+	def calculate_overlap(self):
+		max_overlap=0
+		avg_overlap=0
+		count=0
+		for i in range(0,self.num_classifiers):
+			for j in range(i+1,self.num_classifiers):
+				x1=set(self.classifier_info[i]["label_list"])
+				x2=set(self.classifier_info[j]["label_list"])
+				overlap=len(x1 & x2)
+				avg_overlap+=overlap
+				max_overlap=max(max_overlap,overlap)
+				count+=1
+		avg_overlap=avg_overlap/float(count)
+		dict = {"max":max_overlap,"avg":avg_overlap}
+		return dict
+
+	def training_data_splits(self):
+		splits = []
+		for i in range(0,self.num_classifiers):
+			np=len(self.classifier_info[i]["pos_tset"])
+			nn=len(self.classifier_info[i]["neg_tset"])
+			split=float(np)/float(nn+np)
+			splits.append(split)
+		return splits
 
 
 
@@ -82,14 +164,17 @@ class Ensemble :
 
 def select_pos_training_set(MAX_LABELS,num_labels,all_labels,reverse_dict):
 	#random set of classes
-	labels=[]
+	labels=set()
 	pos_tset=set()
 	while(len(labels)<MAX_LABELS):
 		new_lab=np.random.randint(low=0,high=num_labels,size=1)
 		new_lab=np.asscalar(new_lab)
-		labels.append(new_lab)
+		if new_lab in labels:
+			continue
+		labels.add(new_lab)
 		pos_tset= pos_tset | set(reverse_dict[new_lab])
 
+	labels=sorted(list(labels))
 	pos_tset=sorted(list(pos_tset))
 	return labels,pos_tset
 
